b519c3ff2a46a73af14d8223eb764859412c0e4b
RishavRaj108 <rishavraj4411@gmail.com>
2025-12-15 14:51:40 +0530

    problem detail page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import HomePage from "./pages/HomePage";
 import ProblemsPage from "./pages/ProblemsPage";
 import { Toaster } from "react-hot-toast";
 import DashboardPage from "./pages/DashboardPage";
+import ProblemPage from "./pages/ProblemPage";
 
 function App() {
   const { isSignedIn , isLoaded } = useUser();
@@ -14,12 +15,10 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={ !isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
-        <Route path="/dashboard" element={ isSignedIn ? <DashboardPage/> : <Navigate to={"/"} />} />
-        <Route
-          path="/problems"
-          element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />}
-        />
+        <Route path="/" element={ !isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />}/>
+        <Route path="/dashboard" element={ isSignedIn ? <DashboardPage/> : <Navigate to={"/"} />}/>
+        <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"}/>}/>
+        <Route path="/problem/:id" element={isSignedIn ? <ProblemPage/> : <Navigate to={"/"}/>}/>
       </Routes>
       <Toaster toastOptions={{ duration: 3000}} />
     </>
